384ea835ce19abc8e09f78a3d483a220fd428614
unknown <maxawzsinger@gmail.com>
2022-02-15 15:40:09 +1100

    token fix donate

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,6 +31,10 @@ networks: {
       blockGasLimit: 0x1fffffffffffff,
       allowUnlimitedContractSize: true,
       timeout: 1800000
+    },
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/zM215AwLR7xmXJkJ22jd6dwqxfzG3rJT',
+      accounts : ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
     }
 }
 };
